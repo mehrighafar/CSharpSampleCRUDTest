ed1417faa7c17d9dfe62f15938189d7d989e09d5
mehrighafar <mehri.abdolghafar@gmail.com>
2025-06-07 01:25:01 +0330

    update queries

--- CSharpSampleCRUDTest.DataAccess/DataAccessServices/CustomerDataAccessService.cs
+++ CSharpSampleCRUDTest.DataAccess/DataAccessServices/CustomerDataAccessService.cs
@@ -41,12 +41,6 @@ public class CustomerDataAccessService : ICustomerDataAccessService
     }
     public async Task<CustomerModel> AddAsync(CustomerModel model)
     {
-        if (!await IsEmailUniqueInDb(model.Id, model.Email))
-            return null;
-        else if (!await IsFirstnameLastnameDateofbirthUnique
-            (model.Id, model.FirstName, model.LastName, model.DateOfBirth))
-            return null;
-
         var result = await _customerRepository.AddAsync(_mapper.Map<CustomerEntity>(model));
         if (result is null) { return null; }
 
@@ -54,46 +48,14 @@ public class CustomerDataAccessService : ICustomerDataAccessService
     }
     public async Task<CustomerModel> UpdateAsync(CustomerModel model)
     {
-        if (!await IsEmailUniqueInDb(model.Id, model.Email))
-            return null;
-        else if (!await IsFirstnameLastnameDateofbirthUnique
-            (model.Id, model.FirstName, model.LastName, model.DateOfBirth))
-            return null;
-
         var result = await _customerRepository.UpdateAsync(_mapper.Map<CustomerEntity>(model));
         if (result is null) { throw new CustomerNotFoundException(model.Id); }
 
-        //return _mapper.Map<CustomerModel>(result);
         return model;
     }
     public async Task<bool> DeleteAsync(Guid id)
     {
         var result = await _customerRepository.RemoveAsync(id);
         return result.IsAcknowledged;
-    }
-
-    private async Task<bool> IsEmailUniqueInDb(Guid id, string email)
-    {
-        var customers = await _customerRepository.GetAllAsync();
-        var emails = customers.Where(item => item.Id != id).Select(x => x.Email).ToList();
-        if (emails.Contains(email))
-            return false;
-        return true;
-    }
-
-    private async Task<bool> IsFirstnameLastnameDateofbirthUnique(Guid id, string firstName, string lastName, DateOnly dateOfBirth)
-    {
-        var customers = await _customerRepository.GetAllAsync();
-        var list = customers.Where(item => item.Id != id).Select(item => new
-        {
-            firstName = item.FirstName,
-            lastName = item.LastName,
-            dateOfBirth = item.DateOfBirth
-        }).ToList();
-
-        if (list.Contains(new { firstName, lastName, dateOfBirth }))
-            return false;
-
-        return true;
     }
 }
--- CSharpSampleCRUDTest.DataAccess/Repositories/MongoCustomerRepository.cs
+++ CSharpSampleCRUDTest.DataAccess/Repositories/MongoCustomerRepository.cs
@@ -25,6 +25,12 @@ public class MongoCustomerRepository : ICustomerRepository
 
     public async Task<CustomerEntity> AddAsync(CustomerEntity entity)
     {
+        if (!await IsEmailUniqueInDb(entity.Id, entity.Email))
+            return null;
+        else if (!await IsFirstnameLastnameDateofbirthUnique
+            (entity.Id, entity.FirstName, entity.LastName, entity.DateOfBirth))
+            return null;
+
         entity.Id = Guid.NewGuid();
         await _mongoCollection.InsertOneAsync(entity);
         return entity;
@@ -32,6 +38,12 @@ public class MongoCustomerRepository : ICustomerRepository
 
     public async Task<CustomerEntity?> UpdateAsync(CustomerEntity entity)
     {
+        if (!await IsEmailUniqueInDb(entity.Id, entity.Email))
+            return null;
+        else if (!await IsFirstnameLastnameDateofbirthUnique
+            (entity.Id, entity.FirstName, entity.LastName, entity.DateOfBirth))
+            return null;
+
         var existing = await GetByIdAsync(entity.Id) ?? throw new CustomerNotFoundException(entity.Id);
 
         return await _mongoCollection.FindOneAndReplaceAsync(e => e.Id == entity.Id, entity);
@@ -39,4 +51,27 @@ public class MongoCustomerRepository : ICustomerRepository
 
     public async Task<DeleteResult> RemoveAsync(Guid id) =>
         await _mongoCollection.DeleteOneAsync(e => e.Id == id);
+
+    private async Task<bool> IsEmailUniqueInDb(Guid id, string email)
+    {
+        var filter =
+            Builders<CustomerEntity>.Filter.Ne(u => u.Id, id) &
+            Builders<CustomerEntity>.Filter.Eq(u => u.Email, email);
+
+        var existing = await _mongoCollection.Find(filter).FirstOrDefaultAsync();
+        return existing == null;
+    }
+
+    private async Task<bool> IsFirstnameLastnameDateofbirthUnique(Guid id, string firstName, string lastName, DateOnly dateOfBirth)
+    {
+        var filter =
+            Builders<CustomerEntity>.Filter.Ne(u => u.Id, id) &
+            Builders<CustomerEntity>.Filter.Eq(u => u.FirstName, firstName) &
+            Builders<CustomerEntity>.Filter.Eq(u => u.LastName, lastName) &
+            Builders<CustomerEntity>.Filter.Eq(u => u.DateOfBirth, dateOfBirth);
+
+
+        var existing = await _mongoCollection.Find(filter).FirstOrDefaultAsync();
+        return existing == null;
+    }
 }
